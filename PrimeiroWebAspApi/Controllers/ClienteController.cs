6cc7ac3394b766cd7ba7b19905cf2ecaef577ba2
ipriulit <ipriulit@gmail.com>
2020-05-14 13:06:47 -0300

    Enviando tudo para o github

---
+++ PrimeiroWebAspApi/Controllers/ClienteController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PrimeiroWebAspApi.Model;
+
+namespace PrimeiroWebAspApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        public ClienteController()
+        {
+        }
+
+        // GET: api/Cliente
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            return Repositorio.ClienteRepositorio.Clientes;
+        }
+
+        // GET: api/Cliente/5
+        [HttpGet("{id}", Name = "Get")]
+        public Cliente Get(int id)
+        {
+            return Repositorio.ClienteRepositorio.Clientes.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST: api/Cliente
+        [HttpPost]
+        public void Post([FromBody] Cliente value)
+        {
+            Repositorio.ClienteRepositorio.Clientes.Add(value);
+        }
+
+        // PUT: api/Cliente/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Cliente value)
+        {
+            Cliente cli = Repositorio.ClienteRepositorio.Clientes.FirstOrDefault(x => x.Id == id);
+            if(cli != null)
+            {
+                cli.Nome = value.Nome;
+                cli.Telefone = value.Telefone;
+                cli.Endereco = value.Endereco;
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Cliente cli = Repositorio.ClienteRepositorio.Clientes.FirstOrDefault(x => x.Id == id);
+            if (cli != null)
+            {
+                Repositorio.ClienteRepositorio.Clientes.Remove(cli);
+            }
+        }
+    }
+}
---
+++ PrimeiroWebAspApi/Repositorio/ClienteRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeiroWebAspApi.Repositorio
+{
+    public class ClienteRepositorio
+    {
+        public static List<Model.Cliente> Clientes;
+
+        public static void GeraDados()
+        {
+            ClienteRepositorio.Clientes = new List<Model.Cliente>();
+
+            ClienteRepositorio.Clientes.Add(new Model.Cliente()
+            {
+                Endereco = "Rua Matilda, 101",
+                Id = 1,
+                Nome = "Cliente 1",
+                Telefone = "40028922"});
+
+            ClienteRepositorio.Clientes.Add(new Model.Cliente()
+            {
+                Endereco = "Rua ruan, 100",
+                Id = 2,
+                Nome = "Cliente 2",
+                Telefone = "40023422"
+            });
+
+            ClienteRepositorio.Clientes.Add(new Model.Cliente()
+            {
+                Endereco = "Av. ruan, 900",
+                Id = 3,
+                Nome = "Cliente 3",
+                Telefone = "40023422"
+            });
+        }
+    }
+}
